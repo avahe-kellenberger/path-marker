a4b5a5e0a689f1f5ff03850b749ab4a4628465dc
Patrick Nobbe <patrick.nobbe@hotmail.com>
2024-05-26 16:57:30 +0200

    Update PathMarkerOverlay.java

--- src/main/java/com/pathmarker/PathMarkerOverlay.java
+++ src/main/java/com/pathmarker/PathMarkerOverlay.java
@@ -144,7 +144,7 @@ public class PathMarkerOverlay extends Overlay
         {
             return;
         }
-        final Point screenPoint = Perspective.localToCanvas(client, lp, client.getTopLevelWorldView().getPlane());
+        final Point screenPoint = Perspective.localToCanvas(client, lp, client.getLocalPlayer().getWorldView().getPlane());
 
         if (screenPoint != null)
         {
